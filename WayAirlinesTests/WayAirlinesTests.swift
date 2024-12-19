066a4930f90698cf1573d33ddb39bafa129f98e5
Kelvin Batista Machado <kelvinmachado@live.com>
2024-12-18 21:48:53 -0300

    chore: renamed project files and components for consistency

---
+++ WayAirlinesTests/WayAirlinesTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import WayAirlines
+
+final class WayAirlinesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
--- WayAirlinesUITests/WayAirlinesUITestsLaunchTests.swift
+++ WayAirlinesUITests/WayAirlinesUITestsLaunchTests.swift
@@ -1,13 +1,6 @@
-//
-//  TesteVaDeTaxiUITestsLaunchTests.swift
-//  TesteVaDeTaxiUITests
-//
-//  Created by Kelvin Batista Machado on 05/10/24.
-//
-
 import XCTest
 
-final class TesteVaDeTaxiUITestsLaunchTests: XCTestCase {
+final class WayAirlinesUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
@@ -20,9 +13,6 @@ final class TesteVaDeTaxiUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
